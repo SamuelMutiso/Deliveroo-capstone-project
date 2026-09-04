fe600e2df687973c1035bee5946a6d719dab0fd1
SamuelMutiso <muuosamuel335@gmail.com>
2026-09-04 08:48:02 +0000

    Issue and check confirmation codes

--- server/app/services/__init__.py
+++ server/app/services/__init__.py
@@ -1,4 +1,13 @@
 from . import mailer, maps, mpesa, pricing, sms
-from . import notifications, onboarding
+from . import notifications, onboarding, verification
 
-__all__ = ["mailer", "maps", "mpesa", "notifications", "onboarding", "pricing", "sms"]
+__all__ = [
+    "mailer",
+    "maps",
+    "mpesa",
+    "notifications",
+    "onboarding",
+    "pricing",
+    "sms",
+    "verification",
+]
